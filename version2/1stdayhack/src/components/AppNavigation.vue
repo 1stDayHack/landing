484850ebc56d8f078f4ebc7b486fef65b0a28be4
csleong98 <macintoshleong@gmail.com>
2019-10-15 23:11:29 +0800

    added multiple pages

---
+++ version2/1stdayhack/src/components/AppNavigation.vue
@@ -0,0 +1,55 @@
+<template>
+        <v-app-bar app color="blue accent-3" dark>
+            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-spacer class="d-block d-md-none"></v-spacer>
+            <v-toolbar-title class="headline">
+                <div class="pt-2 d-inline-block">
+                    {{appTitle}}
+                </div>
+            </v-toolbar-title>
+		    <v-spacer></v-spacer>
+            <v-toolbar-items class="mx-2 d-none d-md-block">
+                <v-btn text>About</v-btn>
+                <v-btn text>Tracks</v-btn>
+                <v-btn text>Schedule</v-btn>
+                <v-btn text>Sponsorship</v-btn>
+                <v-btn text>The Team</v-btn>
+
+                <div class="d-inline-block">
+                    <v-btn class="mx-2" color="pink accent-3">SIGN UP</v-btn>
+                </div>
+            </v-toolbar-items>
+
+            <v-navigation-drawer app v-model="drawer" absolute temporary>
+                <v-list dense>
+                    <v-list-item v-for="(item, index) in items" :key="index">
+                        <v-list-item-content>
+                            {{item.title}}
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-list>
+            </v-navigation-drawer>
+        </v-app-bar>
+</template>
+
+<script>
+export default {
+    name: 'AppNavigation',
+    data() {
+        return {
+            appTitle: '1stDayHack',
+            drawer: false,
+            items: [
+                { title: 'About' },
+                { title: 'Tracks' },
+                { title: 'Schedule' },
+                { title: 'Sponsorship' },
+                { title: 'The Team' },
+            ]
+        };
+    }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ version2/1stdayhack/src/components/HomeDetails.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-container>
+        <v-layout-column>
+            <v-flex class="display-2 text-xs-center my-5">Title</v-flex>
+            <v-flex>
+                <div class="headline mt-3">lorem ipsum</div>
+                <p class="subheading mt-3">lorem ipsum</p>
+            </v-flex>
+        </v-layout-column>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'HomeDetails'
+}
+</script>
+
+<style scoped>
+
+</style>
